d05993ebac8aa27aeb9e27ab5051b6096c32bf61
Rajesh Pasupuleti <rajeshpasupuleti5424@gmail.com>
2023-06-03 09:53:13 +0000

    Second Commit

--- login.php
+++ login.php
@@ -54,6 +54,7 @@
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+ <body bgcolor = "#FFFFFF">
 				
             <div style = "margin:30px">
                
